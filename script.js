a99cb0ded3849d39ea700e92e08c7a3279cff34b
DavidHart05 <>
2021-04-30 18:43:40 +0000

    spacing

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 // adds an event listener to the button and, when pressed, displays the message "You pressed the button!"
-document.getElementById('mybutton').addEventListener('click', function(){alert('You pressed the button!')})
+document.getElementById('mybutton').addEventListener('click', function () { alert('You pressed the button!') })
